36873ef1471909534cb9f1904d448966cadd3bbd
bxo11 <krzysiek.klim1999@gmail.com>
2022-12-14 21:56:58 +0100

    lab2 update

--- lab2/markov.py
+++ lab2/markov.py
@@ -1,3 +1,5 @@
+import copy
+import random
 from math import sqrt
 
 from matplotlib import pyplot as plt
@@ -10,12 +12,13 @@ def matrix_sum(matrix):
             sum += e
     return sum
 
+
 def matrix_max(matrix):
     max = 0
     for ee in matrix:
         for e in ee:
             if e > max:
-                max=e
+                max = e
     return max
 
 
@@ -43,7 +46,6 @@ def A():
     x = []
     y = []
     while (abs(matrix_max(P) - matrix_max(P_prev)) > e):
-
         y.append(matrix_max(P))
         P_prev = P
         P = matrix_multi(P)
@@ -51,13 +53,71 @@ def A():
 
     plt.plot(x, y, marker='o')
     plt.xlabel('N')
-    plt.ylabel('suma elementow macierzy P')
+    plt.ylabel('roznica sum elementow macierzy P')
+
+    for pi in P[0]:
+        y = [pi for i in range(len(y))]
+        plt.plot(x, y, marker='x')
 
     plt.show()
 
 
 def B():
-    pass
+    P = [[0.64, 0.32, 0.04], [0.4, 0.5, 0.1], [0.25, 0.50, 0.25]]
+    N = 10 ** 4
+    values = [0, 1, 2]
+
+    for node in values:
+        visits = [0, 0, 0]
+        for i in range(N):
+            choice = node
+            weights = P[choice]
+            choice = random.choices(values, weights)[0]
+            visits[choice] += 1
+        print(f'start: {node} = {visits[0] / N} {visits[1] / N} {visits[2] / N}')
 
 
-A()
+def CD():
+    users = 100
+    start_logged_in_users = random.randint(0, 100)
+    login_probability = 0.2
+    logout_probability = 0.5
+    # logout_probability = 1 - (0.008 * start_logged_in_users + 0.1)
+    N = 10 ** 4
+
+    visits = [0 for u in range(users)]
+
+    logged_in_users = start_logged_in_users
+    for n in range(N):
+        for user in range(users - logged_in_users):
+            if random.random() < login_probability:
+                logged_in_users += 1
+
+        for user in range(logged_in_users):
+            if random.random() < logout_probability:
+                logged_in_users -= 1
+
+        visits[logged_in_users] += 1
+
+    top_five = []
+    temp_values = copy.deepcopy(visits)
+    for i in range(5):
+        maximum = max(temp_values)
+        top_five.append(maximum)
+        temp_values.remove(maximum)
+    plt.plot([i for i in range(5)], top_five, marker='o')
+    plt.title(f'start: {start_logged_in_users}')
+    plt.xlabel('')
+    plt.ylabel('')
+
+    plt.show()
+
+    plt.plot([i for i in range(users)], visits, marker='o')
+    plt.title(f'start: {start_logged_in_users}')
+    plt.xlabel('N')
+    plt.ylabel('')
+
+    plt.show()
+
+
+CD()
