deb843acf027e8d91eeaab73bd5b5d8325c15e70
bxo11 <krzysiek.klim1999@gmail.com>
2023-02-01 17:18:39 +0100

    update 4

--- lab4/metoda_kolejnych_zlowien.py
+++ lab4/metoda_kolejnych_zlowien.py
@@ -1,13 +1,11 @@
 import random
 
-N = 100  # inaczej N_j
-t = 20
-look_repeat = 5
+N = 100  # inaczej N_j / populacja
+t = 20 # ilosc osobnikow zlowionych w danym pomiarze
+repeats = 5
 population_list = [x for x in range(N)]
-repeats_dict = {}  # calkowita ilosc wylosowanych osobnikow i powtorzenia # suma wartosci jest = M
+repeats_dict = {n: 0 for n in range(N)}  # calkowita ilosc wylosowanych osobnikow i powtorzenia
 bayes = 0
-for n in range(N):
-    repeats_dict[n] = 0
 
 new_pool = []
 previous_pool = []
@@ -16,21 +14,28 @@ prior = 0
 likelihood = 0
 posterior = 0
 to_norm = 0
-for i in range(look_repeat):
+M = 0  # calkowita ilosc oznakowamych osob
+m = 0 # liczba ponownie zlowionych osobnikow
+
+for i in range(repeats):
     previous_pool = new_pool
     new_pool = []
-    random.shuffle(population_list)
-    for id in range(t):
-        x = population_list[id]  # zlowiony element
-        new_pool.append(x)
-        repeats_dict[x] = repeats_dict[x] + 1
-
     polled_again = []
-    for element in previous_pool:
-        if element in new_pool:
-            polled_again.append(element)
-    # print(polled_again)
+    M = 0
+    m = 0
+    random.shuffle(population_list)
 
-# for i in repeats_dict:
-#     if repeats_dict[i]>0: print(repeats_dict[i])
-# print(x)
+    for e in range(t):
+        x = population_list[e]  # zlowiony element
+        new_pool.append(x)
+        if x in previous_pool:
+            polled_again.append(x)
+        repeats_dict[x] += 1
+
+    m = len(polled_again)
+
+    for e in repeats_dict:
+        if repeats_dict[e] > 0:
+            M += 1
+
+
